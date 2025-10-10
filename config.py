0f88949d4aaa96c1fc765602f408de438998d320
Ugo Chinemerem <ugofranklin22@gmail.com>
2025-10-09 22:11:06 -0400

    Modular structure

---
+++ config.py
@@ -0,0 +1,6 @@
+SNS_TOPIC_ARN = os.environ.get('SNS_TOPIC_ARN')
+IMAGE_SIZES = {
+    'thumbnail': (150, 150),
+    'medium': (500, 500),
+    'large': (1200, 1200)
+}
---
+++ processor/image_size.py
@@ -0,0 +1,5 @@
+IMAGE_SIZES = {
+    'thumbnail': (150, 150),
+    'medium': (500, 500),
+    'large': (1200, 1200)
+}
